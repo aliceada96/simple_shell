04a09418c068387998fdfc375ba61b062edb2942
Ninolincy <lincyakoth@gmail.com>
2023-05-25 05:38:55 +0300

    Shell cd

--- get_cd.c
+++ get_cd.c
@@ -8,7 +8,9 @@
 int cd_shell(our_shell *arg)
 {
 	char *dir;
-	int home, home2, doubledash;
+	int home = 0;
+	int home2 = 0;
+	int doubledash = 0;
 
 	dir = arg->args[1];
 	if (dir != NULL)
@@ -17,21 +19,25 @@ int cd_shell(our_shell *arg)
 		home2 = _strcmp("~", dir);
 		doubledash = _strcmp("--", dir);
 	}
-	if (dir == NULL || !home || !home2 || !doubledash)
+	else if (dir == NULL || !home || !home2 || !doubledash)
 	{
 		cd_home(arg);
 		return (1);
 	}
-	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
+	else if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
 	{
 		cd_dot(arg);
 		return (1);
 	}
-	if (_strcmp("-", dir) == 0)
+	else if (_strcmp("-", dir) == 0)
 	{
 		cd_previous(arg);
 		return (1);
 	}
-	cd_to(arg);
-	return (1);
+	else
+	{
+		cd_to(arg);
+		return (1);
+	}
+	return (0);
 }
